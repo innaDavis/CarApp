d7f6368866e280f9034abe57e28d28fbf9364087
Inna Davis <idavis@cordelllaw.com>
2019-03-21 16:51:11 -0500

    Add project files.

---
+++ CarApp/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarApp
+{
+    public class Car
+    {
+        public Car(float CurrentGasLevel)
+        {
+            this.CurrentGasLevel = CurrentGasLevel;
+        }
+
+        public static float gasMaxValue = 13;
+
+        public float CurrentGasLevel { get; set; }
+
+        public float AddGas(float value)
+        {
+            return CurrentGasLevel + value;
+        }
+
+        public float FillToMax()
+        {
+            float gasTobeAdded = gasMaxValue - CurrentGasLevel;
+            AddGas(gasTobeAdded);
+
+            return gasTobeAdded;
+        }
+    }
+}
---
+++ CarApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Create a deck cards.
+            float GasAmount = 6.5F;
+            Car gas = new Car(GasAmount);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
